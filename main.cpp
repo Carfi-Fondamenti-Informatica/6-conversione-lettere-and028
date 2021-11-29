08e5a059ecbb827e199b7bca03eadb2c49f9743a
and028 <91878390+and028@users.noreply.github.com>
2021-11-29 08:14:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,21 +7,17 @@ int main() {
 
     cin>>a;
 
+    conversione(a);
+
     if (((a>=65)&&(a<=90)) || ((a>=97)&&(a<=122))) {
 
         cout<<conversione(a)<<endl;
     } else {
 
         cout<<"errore"<<endl;
+
     }
-
-
-
-
-
-
 
 
     return 0;
 }
-
