f7008408beef3d569d270bb8b33302f9780d7d9f
and028 <91878390+and028@users.noreply.github.com>
2021-11-29 08:02:19 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,32 @@
+#include "lib.h"
+
+char conversione (char a) {
+
+    if (((a>=65)&&(a<=90)) || ((a>=97)&&(a<=122))) {
+
+        if ((a>=65)&&(a<=90)) {
+            char risultato=0;
+            risultato=a+32;
+            return risultato;
+
+        }
+
+        if ((a>=97)&&(a<=122)) {
+            char risultato = 0;
+            risultato = a-32;
+            return risultato;
+        }
+
+
+        return 49;
+
+
+    } else {
+       return 48;
+    }
+
+
+
+
+}
 
